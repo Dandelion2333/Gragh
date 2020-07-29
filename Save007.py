79b3537fc3c4ca2afe0829e16b90a1434295b4ce
Dandelion <494933900@qq.com>
2020-07-29 15:56:09 +0800

    1、完成拯救007的项目（基本功能都实现） 2、添加打印DFS算法时007的主要跳转点

--- Save007.py
+++ Save007.py
@@ -3,31 +3,51 @@
 #            已知007的跳跃半径为Jump，鳄鱼坐标存在list中，问题为，007是否能够跳到岸上？输出yes or no
 
 def Save007_DFS(list, start, jump):
+    # 设置递归出口
+    if IsOutput(start) == True:
+        print("start",start)
+        return True
+
     answer = False
+    start[2] = 1    # 标记此点被访问过
+    print("start",start)
     # 循环遍历list中所有的顶点
     for cnt in range(len(list)):
         # 获取以顶点为圆心，跳跃距离为半径，落在圆内的点
-        if IsJump(start, list[cnt]) == True :
+        if (IsJump(start, list[cnt], jump) == True) and (list[cnt][2] == 0):   
+            #print("list", cnt, list[cnt])         
             answer = Save007_DFS(list, list[cnt], jump)
             if answer == True:
                 break
-    if answer == True:
+            else:
+                print("start",start)
+    
+    return answer
+
+def IsJump(start, list, jump):
+    num = (start[0] - list[0]) * (start[0] - list[0]) + ((start[1] - list[1]) * (start[1] - list[1]))
+    num_sqrt = num ** 0.5
+    #print("IsJump",start,list,num,num_sqrt)
+    # 判断是否能调到鳄鱼身上
+    if num_sqrt <= jump:
+        return True
+    else:
+        return False
+
+def IsOutput(start):
+    if (start[0] - jump <= 0) or (start[0] + jump >= 10) or (start[1] - jump <= 0) or (start[1] + jump >= 10):
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    list = [[1,2,0],[3,4,0],[9,5,0],[6,4,0],[7,7,0],[3,5,0],[1,4,0],[4,4,0],[2,3,0]]
+    start = [5,5,0]
+    jump = 2
+
+    answer_dfs = Save007_DFS(list, start, jump)
+
+    if answer_dfs == True:
         print("yes")
     else:
         print("no")
-
-def Dfs(start, list):
-
-
-def IsJump(start, list):
-
-    return True
-
-
-if __name__ == "__main__":
-    list = [[1,2],[3,4],[9,5],[6,4],[7,7],[2,5],[1,4]]
-    start = [5,5]
-    jump = 3
-
-
-    answer_dfs = Save007_DFS(list, start, jump)
